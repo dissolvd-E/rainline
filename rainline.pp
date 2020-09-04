2fe3c8443b41eec30f20cde4c3fde7e2fc17ad72
dissolvd-E <70769362+dissolvd-E@users.noreply.github.com>
2020-09-04 18:47:53 +0200

    minor

--- rainline.pp
+++ rainline.pp
@@ -1,195 +1,195 @@
 
 
 
-    Program Rainline;
-    uses    Crt;
+Program Rainline;
+uses    Crt;
 
 
 
-    type
-        memType = array [Word] of Word;
+type
+    memType = array [Word] of Word;
 
 
 
-    var
-        memory:         memType;
-        userCommand:    Char;
-        threadIterator: Word;
+var
+    memory:         memType;
+    userCommand:    Char;
+    threadIterator: Word;
 
 
 
-    procedure createThread( location: Word );
-    var
-        here: Word;
+procedure createThread( location: Word );
+var
+    here: Word;
+begin
+
+    { find an empty location for the new thread }
+    here := 0;
+
+    { while there's a thread here }
+    while ( memory[here] > 0 )
+
+    { and this thread's not marked as terminated }
+    and ( here <> memory[here] )
+
+    { keep searching }
+    do here += 1;
+    
+    { then create new thread here }
+    memory[here] := location;
+
+end;
+
+
+
+procedure stepThread;
+var
+    address:        Word;
+    source:         Word;
+    destination:    Word;
+    fork:           Word;
+begin
+
+    address :=      memory[threadIterator];
+
+    source :=       memory[address];
+    destination :=  memory[address + 1];
+    fork :=         memory[address + 2];
+
+    if source <> destination then
     begin
 
-        { find an empty location for the new thread }
-        here := 0;
+        { copy content of address A to address B }
+        memory[destination] := memory[source];
 
-        { while there's a thread here }
-        while ( memory[here] > 0 )
+        { let parent jump to the next instruction }
+        memory[threadIterator] += 3;
 
-        { and this thread's not marked as terminated }
-        and ( here <> memory[here] )
+        { fork if it's not where parent is }
+        if fork <> memory[threadIterator] then createThread( fork );
 
-        { keep searching }
-        do here += 1;
+    end else
+    
+        { mark this thread as terminated }
+        memory[threadIterator] := threadIterator;
+
+end;
+
+
+
+procedure step;
+begin
+
+    threadIterator := 0;
+    
+    while memory[threadIterator] > 0 do
+    begin
+
+        { if this thread is not marked as terminated }
+        if memory[threadIterator] <> threadIterator then
         
-        { then create new thread here }
-        memory[here] := location;
+            stepThread;
 
+        threadIterator += 1;
     end;
+end;
 
 
 
-    procedure stepThread;
-    var
-        address:        Word;
-        source:         Word;
-        destination:    Word;
-        fork:           Word;
-    begin
+procedure input;
+var
+    address:        Word;
+    source:         Word;
+    destination:    Word;
+    fork:           Word;
+begin
 
-        address :=      memory[threadIterator];
+    write('Address ');
+    readln( address );
 
-        source :=       memory[address];
-        destination :=  memory[address+1];
-        fork :=         memory[address+2];
+    if address mod 3 <> 0 then
 
-        if source <> destination then
-        begin
+        writeln('Error: Wrong alignment')
 
-            { copy content of address A to address B }
-            memory[destination] := memory[source];
+    else begin
 
-            { let parent jump to the next instruction }
-            memory[threadIterator] += 3;
+        write('Source Destination Fork (space-separated) ');
+        readln( source, destination, fork );
 
-            { fork if it's not where parent is }
-            if fork <> memory[threadIterator] then createThread(fork);
-
-        end else
-        
-            { mark this thread as terminated }
-            memory[threadIterator] := threadIterator;
-
+        memory[address] :=      source;
+        memory[address + 1] :=  destination;
+        memory[address + 2] :=  fork;
     end;
+end;
 
 
 
-    procedure step;
-    begin
+procedure wordInput;
+var
+    address: Word;
+    content: Word;
+begin
 
-        threadIterator := 0;
-        
-        while memory[threadIterator] > 0 do
-        begin
+    write('Address Content (space-separated) ');
+    readln( address, content );
 
-            { if this thread is not marked as terminated }
-            if memory[threadIterator] <> threadIterator then
-            
-                stepThread;
+    memory[address] := content;
+end;
 
-            threadIterator += 1;
-        end;
+
+
+procedure output;
+var
+    start:  Word;
+    length: Word;
+    i:      Word;
+    addr:   Word;
+begin
+
+    write('Dump-start ');
+    readln( start );
+
+    write('Dump-length ');
+    readln( length );
+
+    if start mod 3 <> 0 then
+
+        writeln('Error: Wrong alignment')
+
+    else for i := 0 to length - 1 do begin
+
+        addr := start + i * 3;
+        writeln(
+            '  [',
+            addr,
+            ']  Source:',
+            memory[addr],
+            '  Destination:',
+            memory[addr + 1],
+            '  Fork:',
+            memory[addr + 2]
+        );
     end;
+end;
 
 
 
-    procedure input;
-    var
-        address:        Word;
-        source:         Word;
-        destination:    Word;
-        fork:           Word;
-    begin
+begin
 
-        write('Address ');
-        readln(address);
+    repeat
 
-        if address mod 3 <> 0 then
+        writeln('(w)ord-input  (i)nstruction-input  (o)utput  (s)tep  (q)uit');
+        userCommand := readkey;
 
-            writeln('Error: Wrong alignment')
+        if userCommand = 'w' then wordInput;
 
-        else begin
+        if userCommand = 'i' then input;
 
-            write('Source Destination Fork (space-separated) ');
-            readln(source, destination, fork);
+        if userCommand = 'o' then output;
 
-            memory[address] :=      source;
-            memory[address + 1] :=  destination;
-            memory[address + 2] :=  fork;
-        end;
-    end;
+        if userCommand = 's' then step;
 
-
-
-    procedure wordInput;
-    var
-        address: Word;
-        content: Word;
-    begin
-
-        write('Address Content (space-separated) ');
-        readln(address, content);
-
-        memory[address] := content;
-    end;
-
-
-
-    procedure output;
-    var
-        start:  Word;
-        length: Word;
-        i:      Word;
-        addr:   Word;
-    begin
-
-        write('Dump-start ');
-        readln(start);
-
-        write('Dump-length ');
-        readln(length);
-
-        if start mod 3 <> 0 then
-
-            writeln('Error: Wrong alignment')
-
-        else for i := 0 to length - 1 do begin
-
-            addr := start + i * 3;
-            writeln(
-                '  [',
-                addr,
-                ']  Source:',
-                memory[addr],
-                '  Destination:',
-                memory[addr + 1],
-                '  Fork:',
-                memory[addr + 2]
-            );
-        end;
-    end;
-
-
-
-    begin
-
-        repeat
-
-            writeln('(w)ord-input  (i)nstruction-input  (o)utput  (s)tep  (q)uit');
-            userCommand := readkey;
-
-            if userCommand = 'w' then wordInput;
-
-            if userCommand = 'i' then input;
-
-            if userCommand = 'o' then output;
-
-            if userCommand = 's' then step;
-
-        until userCommand = 'q';
-    end.
+    until userCommand = 'q';
+end.
 
 
